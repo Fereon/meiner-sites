49579a7a6dcda402daa0576fa25f88ef2856b9ec
Lorenz Zahn <argolo.lorenz@googlemail.com>
2021-10-21 14:57:45 +0200

    fixed environ

--- create-environ.sh
+++ create-environ.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 source site-env/bin/activate
+echo "DEBUG=True" > meinersite/meinersite/.env
 echo "import secrets
 print('SECRET_KEY=' + secrets.token_urlsafe(256))
-" | python > meinersite/secret.txt
+" | python >> meinersite/meinersite/.env
 
 deactivate
